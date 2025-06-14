0745f2b7e5caa2c2aaa7b6a0a2a665a8b2900c29
TheBlackKnight9 <codedemon27@gmail.com>
2025-06-15 01:16:39 +0530

    server.js to  structure and ensure database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,24 @@
-import express from "express";
 import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use("/api/auth", authRoutes)
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-    connectDB();
+app.use("/api/auth", authRoutes);
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`);
+    });
+}).catch((error) => {
+    console.error("Failed to connect to database", error);
+    process.exit(1);
 });
