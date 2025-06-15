a47e364a48faf44fe66a88e91bc53a7ce10de98e
Lavesh73 <jangidlavesh733@gmail.com>
2025-06-15 16:04:39 +0530

    feat: integrate Tailwind CSS and restructure App component with routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'  // ✅ import Tailwind here
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
+    </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
